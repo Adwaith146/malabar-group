4fb9cd95f376a491b8156d55613f8651dc8eb457
Adwaith146 <adwaithmkumar@gmail.com>
2025-03-06 21:11:59 -0500

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,150 @@
+import type { NextPage } from 'next';
+import Image from "next/image";
+import styles from "../styles/index.module.css"; // If using Pages Router 
+
+
+const Frame:NextPage = () => {
+  	return (
+    		<div className={styles.groupParent}>
+      			<div className={styles.desktop2Wrapper}>
+        				<div className={styles.desktop2}>
+          					<div className={styles.firstPage}>
+            						<div className={styles.desktop3}>
+              							<div className={styles.home}>
+                								<div className={styles.home1}>Home</div>
+              							</div>
+              							<div className={styles.home2}>
+                								<div className={styles.home3}>{`About `}</div>
+              							</div>
+              							<div className={styles.home4}>
+                								<div className={styles.home5}>{`Contact `}</div>
+              							</div>
+              							<div className={styles.home6}>
+                								<div className={styles.home7}>Services</div>
+              							</div>
+            						</div>
+            						<div className={styles.rentalsWrapper}>
+              							<div className={styles.rentals}>
+										  <Image className={styles.nice1Icon} width={1440} height={1378} alt="Nice Image" src="/Nice_1.png" />
+                								<div className={styles.rentalsChild} />
+              							</div>
+            						</div>
+            						<div className={styles.firstPageInner}>
+									<Image width={130} height={132} alt="Logo" src="/Group_15.png" />
+            						</div>
+            						<div className={styles.welcomeToMalabarFamilyWrapper}>
+              							<div className={styles.welcomeToMalabar}>Welcome to Malabar Family</div>
+            						</div>
+            						<div className={styles.firstPageChild}>
+              							<Image className={styles.groupItem} width={459} height={304} alt="" src="Group 7.png" />
+            						</div>
+          					</div>
+          					<div className={styles.getToKnowUs}>
+            						<div className={styles.getToParent}>
+              							<b className={styles.getTo}>{`GET TO `}</b>
+              							<b className={styles.knowUs}>KNOW US</b>
+              							<div className={styles.groupInner} />
+            						</div>
+            						<div className={styles.malabarGroupIsATrustedProWrapper}>
+              							<div className={styles.malabarGroupIs}>Malabar Group is a trusted property management company based in Guelph, Ontario since 2020. We take care of different types of properties, like houses, apartments, and commercial spaces. Our team handles repairs, maintenance, and day-to-day tasks to keep everything in good shape. We work hard to make sure property owners feel supported and tenants are happy. We&apos;re here to solve problems quickly and keep things running smoothly.</div>
+              							</div>
+              							<div className={styles.frameParent}>
+                								<div className={styles.frameDiv} />
+                								<Image className={styles.annieSprattTj8x4ocq5jeUnsplIcon} width={397} height={304} alt="" src="annie-spratt-tJ8x4oCQ5jE-unsplash 1.png" />
+              							</div>
+              							<div className={styles.ourGoalIs}>Our goal is to take the stress out of managing properties. Malabar Group offers friendly and professional service you can count on. We care about building strong relationships with our clients. Choose us for simple, dependable property management.</div>
+            						</div>
+            						<div className={styles.whatWeHave}>
+              							<div className={styles.weProvideA}>We provide a wide range of services, including PROPERTY rentals, ensuring reliable and affordable transportation solutions. Our car wash service delivers top-quality cleaning for a spotless finish. We also offer gardening services to enhance outdoor spaces, along with fencing solutions for security and privacy, ensuring a well-maintained property.</div>
+              							<b className={styles.whatWeHave1}>{`WHAT WE HAVE  `}</b>
+            						</div>
+            						<div className={styles.visitUs}>
+              							<div className={styles.visitUsInner}>
+                								<div className={styles.icons8Rent641Parent}>
+                  									<Image className={styles.icons8Rent641} width={64} height={64} alt="" src="icons8-rent-64 1.png" />
+                  									<div className={styles.weOfferReliable}>We offer reliable and affordable property rentals for residential and commercial needs.</div>
+                  									<div className={styles.rectangleWrapper}>
+                    										<div className={styles.rectangleDiv} />
+                  									</div>
+                  									<div className={styles.visitUs1}>
+                    										<div className={styles.visitUs2}>VISIT US</div>
+                  									</div>
+                								</div>
+              							</div>
+              							<div className={styles.visitUsChild}>
+                								<div className={styles.groupContainer}>
+                  									<div className={styles.rectangleContainer}>
+                    										<div className={styles.rectangleDiv} />
+                  									</div>
+                  									<Image className={styles.image5Icon} width={64} height={64} alt="" src="image 5.png" />
+                  									<div className={styles.weOfferExpert}>We offer expert landscaping services, enhancing outdoor spaces with beauty AND functionality.</div>
+                  									<div className={styles.visitUs3}>
+                    										<div className={styles.visitUs2}>VISIT US</div>
+                  									</div>
+                								</div>
+              							</div>
+              							<div className={styles.visitUsInner1}>
+                								<div className={styles.weOfferReliableAndAffordabParent}>
+                  									<div className={styles.weOfferReliable}>We offer reliable and affordable property rentals for residential and commercial needs.</div>
+                  									<Image className={styles.icons8Fence1001} width={70} height={70} alt="" src="icons8-fence-100 1.png" />
+                  									<div className={styles.rectangleFrame}>
+                    										<div className={styles.rectangleDiv} />
+                  									</div>
+                  									<div className={styles.visitUs5}>
+                    										<div className={styles.visitUs2}>VISIT US</div>
+                  									</div>
+                								</div>
+              							</div>
+              							<div className={styles.groupParent1}>
+                								<div className={styles.groupDiv}>
+                  									<div className={styles.rectangleWrapper1}>
+                    										<div className={styles.rectangleDiv} />
+                  									</div>
+                  									<Image className={styles.icons8CarWash641} width={64} height={64} alt="" src="icons8-car-wash-64 1.png" />
+                  									<div className={styles.visitUs7}>
+                    										<div className={styles.visitUs2}>VISIT US</div>
+                  									</div>
+                								</div>
+                								<div className={styles.weProvideProfessionalCarDeWrapper}>
+                  									<div className={styles.weProvideProfessional}>We provide professional car detailing for a spotless, polished, and showroom-quality finish.</div>
+                								</div>
+              							</div>
+            						</div>
+            						<div className={styles.contactUs}>
+              							<b className={styles.contactUs1}>CONTACT US</b>
+              							<div className={styles.getInTouch}>Get in touch for inquiries and reservations.</div>
+              							<div className={styles.contactUsChild} />
+              							<div className={styles.contactUsItem} />
+              							<div className={styles.yourName}>Your Name</div>
+              							<div className={styles.submit}>SUBMIT</div>
+              							<Image className={styles.contactUsInner} width={949} height={3} alt="" src="Vector 2.svg" />
+              							<div className={styles.sliderControlWrapper}>
+                								<div className={styles.sliderControl}>
+                  									<div className={styles.sliderControlChild} />
+                  									<Image className={styles.sliderControlItem} width={25} height={14} alt="" src="Vector 3.svg" />
+                  									<div className={styles.slideToSubmit}> Slide to Submit</div>
+                  									<div className={styles.release}>Release</div>
+                  									<div className={styles.confirmed}>Confirmed!</div>
+                								</div>
+              							</div>
+              							<div className={styles.contactUsChild1} />
+              							<div className={styles.contactUsChild2} />
+              							<div className={styles.yourEmail}>Your Email</div>
+              							<div className={styles.yourNumber}>Your Number</div>
+              							<div className={styles.yourMessage}>Your Message</div>
+              							<Image className={styles.icons8Instagram501} width={64} height={64} alt="" src="icons8-instagram-50 1.png" />
+              							<Image className={styles.icons8Whatsapp501} width={64} height={64} alt="" src="icons8-whatsapp-50 1.png" />
+              							<Image className={styles.icons8Facebook501} width={64} height={64} alt="" src="icons8-facebook-50 1.png" />
+              							<Image className={styles.icons8Gmail641} width={64} height={64} alt="" src="icons8-gmail-64 1.png" />
+            						</div>
+            						<div className={styles.footer}>
+              							<div className={styles.footerChild} />
+              							<div className={styles.malabarResidenceAll}>© 2025 Malabar Residence. All rights reserved.</div>
+            						</div>
+          					</div>
+        				</div>
+      			</div>);
+      			};
+      			
+      			export default Frame;
+      			
